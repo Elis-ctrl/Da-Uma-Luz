d516fd3de00164bfaf987f434916191e1333c251
Carol Shimada <caroline_shimada@hotmail.com>
2021-04-14 14:47:54 -0300

    input qualidade do ar ok

--- src/services/api.js
+++ src/services/api.js
@@ -1,32 +1,30 @@
-// let input = document.getElementsByClassName('input').value;
-// let button = document.getElementsByClassName('submit');
-// let show = document.getElementsByClassName('result')
-
 const ListPolutionCities = async (cidade) => {
+  let airQuality = '';
 
-  fetch('https://api.waqi.info/feed/' + cidade + '/?token=2a8aa0c29b06e31c01bffc261d84b76dc7df73be')
-  .then(response => response.json())
-  .then(json => {
-    console.log(json.data);
-    const aqi = json.data.aqi;
-    if (aqi >= 0 && aqi <= 40) {
-      console.log('Qualidade do ar: Boa');
-    } else if (aqi >= 41 && aqi <= 80) {
-      console.log('Qualidade do ar: Moderada');
-    } else if (aqi >= 81 && aqi <= 120) {
-      console.log('Qualidade do ar: Ruim');
-    } else if (aqi >= 121 && aqi <= 200) {
-      console.log('Qualidade do ar: Muito Ruim');
-    } else if (aqi >= 201) {
-      console.log('Qualidade do ar: Péssima');
-    } else {
-      console.log('erro');
-    }
-  })
-  .catch(erro => console.error(erro.message));
-}
+  // eslint-disable-next-line prefer-template
+  await fetch('https://api.waqi.info/feed/' + cidade + '/?token=2a8aa0c29b06e31c01bffc261d84b76dc7df73be')
+    .then((response) => response.json())
+    .then((json) => {
+      const aqi = json.data.aqi;
+      if (aqi >= 0 && aqi <= 40) {
+        airQuality = 'Qualidade do ar: Boa';
+      } else if (aqi >= 41 && aqi <= 80) {
+        airQuality = 'Qualidade do ar: Moderada';
+      } else if (aqi >= 81 && aqi <= 120) {
+        airQuality = 'Qualidade do ar: Ruim';
+      } else if (aqi >= 121 && aqi <= 200) {
+        airQuality = 'Qualidade do ar: Muito Ruim';
+      } else if (aqi >= 201) {
+        airQuality = 'Qualidade do ar: Péssima';
+      } else {
+        alert('Verifique se nome da cidade esta escrita corretamente');
+      }
+    })
+    .catch((erro) => console.error(erro.message));
+
+  return airQuality;
+};
 export default ListPolutionCities;
-
 
 /*
 AQI = Índice de Qualidade do Ar
@@ -36,4 +34,6 @@ AQI = Índice de Qualidade do Ar
 81 – 120: Ruim
 121 – 200: Muito Ruim
 > 200: Péssima
+
+https://servicos.cetesb.sp.gov.br/qa/
 */
