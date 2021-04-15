43977ffff0906810714e5aa326b37a039e390943
apretamayara <mayara.oliveirapessoal@gmail.com>
2021-04-14 21:51:51 -0300

    home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,17 @@
 export const Home = () => {
   const rootElement = document.createElement('div');
-  Home.classList.add('container');
 
-  rootElement.innerHTML = `
+  rootElement.innerHTML = ` 
       <aside class='home-element'>
-      <p>Texto dentro do elemento</p>
-      </aside>
+      </aside>  
+      <div id="main-container" >
+      <h1>Descubra tudo sobre energia renovável e qual a melhor opção para seu negócio ser mais sustentável</h1>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+
   `;
   return rootElement;
 };
